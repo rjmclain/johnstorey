7459e329f4b178bf4c7dc820bdb19902fd4e2f10
John Storey <john.storey@insureon.com>
2017-10-10 12:08:45 -0700

    Have full, expected API working with serverless local function tests.
    - list-instances
    - deregister-target
    - register-target

---
+++ backend/register-target.js
@@ -0,0 +1,28 @@
+import AWS from "aws-sdk";
+import { success, failure } from "./libs/response-lib";
+
+AWS
+  .config
+  .update({region: "us-east-1"});
+
+export function main(event, context, callback) {
+  const data = JSON.parse(event.body);
+  const elbv2 = new AWS.ELBv2();
+  const params = {
+    TargetGroupArn: data.group,
+    Targets: [
+    {
+      Id: data.id,
+    }
+    ]
+  };
+
+  elbv2.registerTargets(params, function (err, data) {
+    if (err) {
+      //callback(null, failure({ status: false }));
+      callback(null, failure(err));
+    } else {
+      callback(null, success(data));
+    }
+  });
+}
