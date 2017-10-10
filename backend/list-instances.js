ffaa8e8830cd02170f48ff2f0632b4a10fa338e1
John Storey <johnstorey@The-Spanish-Inquisition.local>
2017-10-10 08:15:34 -0700

    list-instances implemented.
    - Tested with serverless invoke.
    - User added manually to Cognito user pool.

---
+++ backend/list-instances.js
@@ -0,0 +1,24 @@
+import AWS from "aws-sdk";
+import { success, failure } from "./libs/response-lib";
+
+AWS
+  .config
+  .update({region: "us-east-1"});
+
+export function main(event, context, callback) {
+  var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
+
+  // Set response headers to enable CORS.
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": true
+  };
+
+  ec2.describeInstances({}, function (err, data) {
+    if (err) {
+      callback(null, failure({ status: false }));
+    } else {
+      callback(null, success(data));
+    }
+  });
+}
