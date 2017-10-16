80bd0c5fe863d918cc89263233c484b6ceb582c4
John Storey <john.storey@insureon.com>
2017-10-16 11:20:16 -0700

    Added path for UATToProd
    - copy of the deploy page for now.
    - adding in items to nav bar.

---
+++ frontend/src/containers/UATToProd.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { invokeApig, authUser, signOutUser } from "../libs/awsLib";
+import config from "../config";
+import "./Home.css";
+import Instances from "./Instances";
+import Deployed from "./Deployed";
+
+export default class UATToProd extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deregister: this.deregister,
+      deployed: []
+    }
+  }
+
+  async deregister(instanceId) {
+    await invokeApig({
+      path: "/deregister",
+      method: "POST",
+      headers: {},
+      queryParams: {},
+      body: {id: instanceId, group: config.ec2.TARGET_GROUP }
+    })
+  }
+
+  setDeployed(deployedList) {
+    this.setState({ deployed: deployedList});
+  }
+
+  render() {
+    return (
+      <div className="Home">
+      <div className="lander">
+      <h1>DNN Deployment</h1>
+      <br />
+      <p>Deployed</p>
+      <Deployed deployed={this.state.deployed} setDeployed={this.setDeployed.bind(this) } />
+      <br />
+      <p>Deployment Candidates</p>
+      <Instances deployed={this.state.deployed}
+        setDeployed={this.setDeployed.bind(this)}
+        deregister={this.deregister.bind(this)}
+        />
+      </div>
+      </div>
+    );
+  }
+}
