2a5fc0ea29c9066b2330878594a2d1c6af77f509
John Storey <john.storey@insureon.com>
2017-10-17 12:08:11 -0700

    Implemented back-end /fetch-amis API code.

---
+++ backend/fetch-amis.js
@@ -0,0 +1,28 @@
+import AWS from "aws-sdk";
+import { success, failure } from "./libs/response-lib";
+
+export function main(event, context, callback) {
+  const data = JSON.parse(event.body);
+
+  AWS
+    .config
+    .update({ region: data.region });
+
+  const ec2 = new AWS.EC2();
+
+  // Set response headers to enable CORS.
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": true
+  };
+
+  ec2.describeImages({
+    Owners: [ "self" ]
+  }, function (err, data) {
+    if (err) {
+      callback(null, failure({ status: false }));
+    } else {
+      callback(null, success(data));
+    }
+  });
+}
