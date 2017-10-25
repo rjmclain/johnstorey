0216b0f92703a7642ccd4a8bfaf398d027a8cc43
John Storey <john.storey@insureon.com>
2017-10-25 08:15:18 -0700

    Imlplemented /describe-image.

---
+++ backend/describe-image.js
@@ -0,0 +1,24 @@
+import AWS from "aws-sdk";
+import {success, failure} from "./libs/response-lib";
+
+export function main(event, context, callback) {
+  const data = JSON.parse(event.body);
+
+  AWS
+    .config
+    .update({ region: data.region });
+
+  const ec2 = new AWS.EC2();
+
+  // Get the instance status and return it.
+  ec2.describeImages({
+    ImageIds: [ data.amiId ],
+    Filters: data.filters,
+  }, function(err, data) {
+    if (err) {
+      callback(null, failure({ status: false, err: err }));
+    } else {
+      callback(null, success(data));
+    }
+  })
+}
