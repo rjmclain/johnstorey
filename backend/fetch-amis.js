a43344850a4ca0c3b3df0dee44ac603e42858191
John Storey <john.storey@insureon.com>
2017-11-10 14:12:47 -0800

    Fixed API /fetch-amis.

--- backend/fetch-amis.js
+++ backend/fetch-amis.js
@@ -1,23 +1,24 @@
-import { success, failure } from './libs/response-lib';
-import AWS from 'aws-sdk';
+import { success, failure } from "./libs/response-lib";
+import AWS from "aws-sdk";
 
 export function main(event, context, callback) {
   const data = JSON.parse(event.body);
 
-  AWS
-    .config
-    .update({ region: data.region });
+  AWS.config.update({ region: data.region });
 
   const ec2 = new AWS.EC2();
 
-  ec2.describeImages({
-    Owners: [ "self" ],
-    Filters: data.filters,
-  }, function (err, data) {
-    if (err) {
-      callback(null, failure({ status: false }));
-    } else {
-      callback(null, success(data));
+  ec2.describeImages(
+    {
+      Owners: ["self"],
+      Filters: data.filters
+    },
+    function(err, data) {
+      if (err) {
+        callback(null, failure({ status: false, err: err }));
+      } else {
+        callback(null, success(data));
+      }
     }
-  });
+  );
 }
