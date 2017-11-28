737fcd28ad92516d342df0b15b5a9299bd516c00
John Storey <john.storey@insureon.com>
2017-11-28 11:11:34 -0800

    Changed 'target-health' api to POST from GET.
    - GET was turned to OPTIONS by Serverless.
    - Custom IAM policy was not respecting Invoke perms on OPTIONS.
    - We probably should be sending the TARGET_GROUP in the body anyways.

--- frontend/src/sagas/fetchDeployedInstancesSaga.js
+++ frontend/src/sagas/fetchDeployedInstancesSaga.js
@@ -2,12 +2,14 @@ import { put } from "redux-saga/effects";
 import * as eventTypes from "../constants/eventTypes";
 import * as instanceSelectActions from "../actions/instanceSelectActions";
 import { invokeApig } from "../libs/awsLib";
+import config from "../config";
 
 export function* fetchDeployedInstancesSaga(action) {
   try {
     const instances = yield invokeApig({
       path: "/target-health",
-      method: "GET"
+      method: "POST",
+      body: { targetGroupId: config.ec2.TARGET_GROUP }
     });
 
     const instanceList = instances.TargetHealthDescriptions.map(instance => {
